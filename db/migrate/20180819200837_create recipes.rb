364c6a5095c2d77a9ca29ce1ddac0a1b07f08c1e
Nate Miller <natekmiller96@gmail.com>
2018-08-19 20:13:20 +0000

    automatically backed up by learn

--- db/migrate/20180819200837_create recipes.rb
+++ db/migrate/20180819200837_create recipes.rb
@@ -1,2 +1,13 @@
 class CreateUsers < ActiveRecord::Migration
-  
+  def up
+    create_table :recipes do |t|
+      t.string :name
+      t.string :ingredients
+      t.string :cook_time
+    end
+  end
+ 
+  def down
+    drop_table :recipes
+  end
+end
