b3cc15e377312acc73cca9b8fd606fd71e58ede9
Nate Miller <natekmiller96@gmail.com>
2018-08-19 20:27:00 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,5 +10,5 @@ class ApplicationController < Sinatra::Base
     erb :new
   end
   post '/recipes'
-    @recipe = Recipe.create(:name => params[:name], )
+    @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], )
 end
